257e6f30c2a1f531132c3f64e6bf9aa6cff85129
RickyTicky <Rickyabrooks92@gmail.com>
2022-11-28 17:03:05 -0500

    added instructions and currentStep function
    Co-authored-by: NessDan <304269+NessDan@users.noreply.github.com>

--- wizard.js
+++ wizard.js
@@ -1,3 +1,14 @@
+const currentStep = () => steps[wizardStep];
+
+const updateInstructions = () => {
+  if (currentStep().message) {
+    instructionsEl.innerText = currentStep().message;
+  } else if (currentStep().message) {
+    instructionsEl.innerText = `Press key for: ${currentStep().keyToSet}`;
+  }
+};
+const instructionsEl = document.getElementById('directions');
+
 const wizardSettings = {
   leftStick: {
     up: '',
@@ -45,18 +56,28 @@ let wizardStep = 0;
 
 const steps = [
   {
-    message: 'Press Key for Left Stick Up',
+    keyToSet: 'Left Stick Up',
     objectReference: wizardSettings.leftStick,
     propertyToModify: 'up',
+  },
+  {
+    keyToSet: 'Left Stick Down',
+    objectReference: wizardSettings.leftStick,
+    propertyToModify: 'down',
+  },
+  {
+    message: "You're all set! Click the button below to save your settings.",
   },
 ];
 
 console.log(wizardSettings);
 
 document.addEventListener('keydown', (event) => {
-  const prop = steps[wizardStep].propertyToModify;
-  const wizardObject = steps[wizardStep].objectReference;
+  const prop = currentStep().propertyToModify;
+  const wizardObject = currentStep().objectReference;
   wizardObject[prop] = event.code;
-
   wizardStep += 1;
+  updateInstructions();
 });
+
+updateInstructions();
