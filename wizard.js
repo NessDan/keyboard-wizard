f60fbeaf0b7423721d58bb9f6a9a189654742fe0
RickyTicky <Rickyabrooks92@gmail.com>
2022-11-28 16:40:09 -0500

    added javascript wizardSettings and input listner
    Co-authored-by: NessDan <304269+NessDan@users.noreply.github.com>

--- wizard.js
+++ wizard.js
@@ -1,1 +1,62 @@
-// code goes here
+const wizardSettings = {
+  leftStick: {
+    up: '',
+    down: '',
+    left: '',
+    right: '',
+  },
+  rightStick: {
+    up: '',
+    down: '',
+    left: '',
+    right: '',
+  },
+  buttons: {
+    a: '',
+    b: '',
+    x: '',
+    y: '',
+  },
+  triggers: {
+    l: '',
+    r: '',
+    zl: '',
+    zr: '',
+  },
+  dpad: {
+    up: '',
+    down: '',
+    left: '',
+    right: '',
+  },
+  system: {
+    start: '',
+    select: '',
+    home: '',
+    capture: '',
+  },
+  misc: {
+    leftStickClick: '',
+    rightStickClick: '',
+  },
+};
+
+let wizardStep = 0;
+
+const steps = [
+  {
+    message: 'Press Key for Left Stick Up',
+    objectReference: wizardSettings.leftStick,
+    propertyToModify: 'up',
+  },
+];
+
+console.log(wizardSettings);
+
+document.addEventListener('keydown', (event) => {
+  const prop = steps[wizardStep].propertyToModify;
+  const wizardObject = steps[wizardStep].objectReference;
+  wizardObject[prop] = event.code;
+
+  wizardStep += 1;
+});
