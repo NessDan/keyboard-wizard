7721ef7d6bbf453db451d014fd44c2a9687f921f
RickyTicky <Rickyabrooks92@gmail.com>
2022-11-29 15:23:21 -0500

    added active cards/updated the text of the keys
    Co-authored-by: NessDan <304269+NessDan@users.noreply.github.com>

--- wizard.js
+++ wizard.js
@@ -7,6 +7,33 @@ const updateInstructions = () => {
     instructionsEl.innerText = `Press key for: ${currentStep().keyToSet}`;
   }
 };
+
+const updateKeyEl = (parentProp, prop) => {
+  const keyEl = document.querySelector(`#${parentProp}-${prop}-key`);
+  keyEl.innerText = wizardSettings[parentProp][prop];
+};
+
+const updateAllKeyEls = () => {
+  Object.keys(wizardSettings).forEach((parentProp) => {
+    Object.keys(wizardSettings[parentProp]).forEach((prop) => {
+      updateKeyEl(parentProp, prop);
+    });
+  });
+};
+
+const setActiveCard = () => {
+  const parentProp = currentStep().parentProp;
+  const prop = currentStep().propertyToModify;
+  const nextActiveCardEl = document.querySelector(
+    `#${parentProp}-${prop}-card`
+  );
+  const prevActiveCardEl = document.querySelector('.card.active');
+  prevActiveCardEl?.classList?.remove('active');
+  prevActiveCardEl?.ariaPressed ?? 'false';
+  nextActiveCardEl?.classList?.add('active');
+  nextActiveCardEl?.ariaPressed ?? 'true';
+};
+
 const instructionsEl = document.getElementById('directions');
 
 const wizardSettings = {
@@ -57,12 +84,12 @@ let wizardStep = 0;
 const steps = [
   {
     keyToSet: 'Left Stick Up',
-    objectReference: wizardSettings.leftStick,
+    parentProp: 'leftStick',
     propertyToModify: 'up',
   },
   {
     keyToSet: 'Left Stick Down',
-    objectReference: wizardSettings.leftStick,
+    parentProp: 'leftStick',
     propertyToModify: 'down',
   },
   {
@@ -73,16 +100,20 @@ const steps = [
 console.log(wizardSettings);
 
 const keyDownHandler = (event) => {
+  event.preventDefault();
   if (wizardStep === steps.length - 1) {
     return;
   }
+  const parentProp = currentStep().parentProp;
   const prop = currentStep().propertyToModify;
-  const wizardObject = currentStep().objectReference;
-  wizardObject[prop] = event.code;
+  wizardSettings[parentProp][prop] = event.code;
   wizardStep += 1;
+  setActiveCard();
+  updateKeyEl(parentProp, prop);
   updateInstructions();
 };
 
 document.addEventListener('keydown', keyDownHandler);
-
+setActiveCard();
 updateInstructions();
+updateAllKeyEls();
